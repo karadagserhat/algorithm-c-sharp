6f8ff5f7004394296e7fce50c81cb3e129372b80
karadagserhat <serhatkaradagg@outlook.com>
2025-03-15 22:04:34 +0300

    Added new solutions

---
+++ Algorithm/16stack.cs
@@ -0,0 +1,39 @@
+﻿namespace Algorithm
+{
+    public class stack<T>
+    {
+        private List<T> data = [];
+
+        public void Add(T record)
+        {
+            data.Insert(0, record);
+        }
+
+        public T Remove()
+        {
+            var item = data[^1];
+            data.RemoveAt(data.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return data[^1];
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(string.Join(", ", data));
+        }
+
+        /*
+         * 
+            var q = new queue<int>();
+        q.Add(1);
+        q.Add(2);
+        q.Add(3);
+        q.Remove();
+        q.Display();
+         */
+    }
+}
