074713fe4a8e13a78a562897c7d54c95312a62b7
karadagserhat <serhatkaradagg@outlook.com>
2025-03-23 17:38:50 +0300

    Added new solutions

---
+++ Algorithm/leetOthersValidParentheses.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithm
+{
+    public class SolutionIsValid
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = [];
+
+            foreach (char c in s)
+            {
+                if (c == '{' || c == '[' || c == '(') stack.Push(c);
+                else
+                {
+                    if (stack.Count == 0) return false; // Kapanış var ama açılış yoksa geçersiz
+
+
+                    char top = stack.Pop();
+
+                    if ((c == '}' && top != '{') ||
+                       (c == ']' && top != '[') ||
+                       (c == ')' && top != '(')) return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
